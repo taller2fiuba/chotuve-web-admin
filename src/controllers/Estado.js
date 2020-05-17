d93a89df43298d77a7f98c928633770ccb2e3291
edjus <ejusto@fi.uba.ar>
2020-05-17 13:16:18 -0300

    agrego ruta estado para ver los estados de los servidores

---
+++ src/controllers/Estado.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class Estado extends Component{
+    render(){
+        return(
+            <div>
+                <p>
+                    Estado de los servers
+                </p>
+            </div>
+        )
+    }
+}
