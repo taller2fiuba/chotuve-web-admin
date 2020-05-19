45c16a13cd8f31cd35054411a292f2d5b4c8ac47
edjus <ejusto@fi.uba.ar>
2020-05-19 00:26:40 -0300

    AuthService agregado y mostrar estado de los app-servers obtenidos del servicio

---
+++ src/comunications/AutServerService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// TODO: Mover a variable de ambiente
+const AUTH_SERVER = 'https://chotuve-auth-server-g4.herokuapp.com';
+
+class AuthServerService{
+
+    getAppServers(callback, errorHandler){
+        // TODO:Debería devolver una lista de objetos con la información de las appservers regitradas
+        axios(`${AUTH_SERVER}/appservers`)
+            .then(response => callback(response))
+            .catch(error => errorHandler(error));
+
+        // callback([{nombre: 'appserver', url: 'https://chotuve-app-server.herokuapp.com'}])
+    }
+}
+
+export default new AuthServerService();
--- src/controllers/Estado.js
+++ src/controllers/Estado.js
@@ -1,8 +1,48 @@
 import React, {Component} from 'react';
-import {FilaEstadoServer} from '../components/FilaEstadoServer'
-import Table from 'react-bootstrap/Table'
+import {FilaEstadoServer} from '../components/FilaEstadoServer';
+import Table from 'react-bootstrap/Table';
+import authServerService from '../comunications/AutServerService';
+
+
+// TODO: Mover a variable de ambiente
+const AUTH_SERVER = 'https://chotuve-auth-server-g4.herokuapp.com';
 
 export class Estado extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            appservers : [],
+        };
+
+        this.handleApiResponse = this.handleApiResponse.bind(this);
+    }
+
+    componentDidMount() {   
+        authServerService.getAppServers(this.handleApiResponse, (error) => console.log(error));
+    }
+
+    handleApiResponse(response) {
+        this.setState({
+            appservers: response,
+        });
+    }
+
+    renderEstadoServers = () =>{
+        const appservers = this.state.appservers;
+
+        return appservers.map(server => {
+            const {nombre, url} = server;
+
+            return(
+                <FilaEstadoServer
+                    key={nombre}
+                    nombre={nombre}
+                    url={url}
+                />
+            )
+        })
+    }
+
     render(){
         return(
             <div>
@@ -13,11 +53,12 @@ export class Estado extends Component{
                         <tr>
                             <th>Servidor</th>
                             <th>Estado</th>
+                            <th>Url</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <FilaEstadoServer server="authserver"></FilaEstadoServer>
-                        <FilaEstadoServer server="appserver"></FilaEstadoServer>
+                        <FilaEstadoServer nombre="authserver" url={AUTH_SERVER}></FilaEstadoServer>
+                        {this.renderEstadoServers()}
                     </tbody>
                 </Table>
             </div>
