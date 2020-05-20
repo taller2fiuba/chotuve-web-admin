48fd6ee34b3860e84b2f59215a6a038cea6e316f
edjus <ejusto@fi.uba.ar>
2020-05-20 00:24:13 -0300

    Remuevo el controlador de Home y cambio rutas

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import {Home} from '../controllers/Home';
 import {Estado} from '../controllers/Estado';
 
 class Routes extends Component {
     render() {
         return (
             <Router key="router">
-                <Route exact path={"/home"} component={Home}/>
-                <Route exact path={"/estado"} component={Estado}/>
+                <Route exact path={"/"} component={Estado}/>
             </Router>
         )
     }
