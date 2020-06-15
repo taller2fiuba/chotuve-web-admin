30f991cf0bdcc00794baef958d0784f5baa0f330
edjus <ejusto@fi.uba.ar>
2020-06-15 01:20:05 -0300

    cambio a tabla de material en pantalla usuarios

--- src/usuarios/PantallaUsuarios.jsx
+++ src/usuarios/PantallaUsuarios.jsx
@@ -1,17 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { Container, Typography } from "@material-ui/core";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
 
-import Table from "react-bootstrap/Table";
-import Alert from "react-bootstrap/Alert";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Alert from "@material-ui/lab/Alert";
 import DoneIcon from "@material-ui/icons/Done";
 import ClearIcon from "@material-ui/icons/Clear";
 
-import ModalEditarUsuario from "./ModalComponents/ModalEditar";
 import ModalDeshabilitarUsuario from "./ModalComponents/ModalDeshabilitar";
+import ModalEditarUsuario from "./ModalComponents/ModalEditar";
 
 import * as AuthServerService from "../comunications/AuthServerService";
 
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
 const PantallaUsuarios = () => {
+  const classes = useStyles();
+
   const [usuarios, setUsuarios] = useState([]);
   const [error, setError] = useState({
     hayError: false,
@@ -32,24 +65,54 @@ const PantallaUsuarios = () => {
     }
   };
 
+  const editarUsuario = (usuarioId, usuario) => {
+    console.log(usuario);
+  };
+
+  const cambiarEstadoUsuario = (usuarioId, estadoViejo) => {
+    console.log(usuarioId);
+    console.log(estadoViejo);
+  };
+
+  const renderTableHeaders = () => {
+    const headers = [
+      "Id",
+      "Nombre",
+      "Apellido",
+      "Email",
+      "Teléfono",
+      "Habilitado",
+      "Acciones",
+    ];
+    return (
+      <TableHead>
+        <TableRow>
+          {headers.map((header) => (
+            <StyledTableCell key={header}>{header}</StyledTableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+    );
+  };
+
   const renderTableBody = () => {
     return (
-      <tbody>
+      <TableBody>
         {usuarios.map((usuario) => (
-          <tr key={usuario.id}>
-            <td>{usuario.id}</td>
-            <td>{usuario.nombre}</td>
-            <td>{usuario.apellido}</td>
-            <td>{usuario.email}</td>
-            <td>{usuario.telefono}</td>
-            <td>
+          <StyledTableRow key={usuario.id}>
+            <StyledTableCell>{usuario.id}</StyledTableCell>
+            <StyledTableCell>{usuario.nombre}</StyledTableCell>
+            <StyledTableCell>{usuario.apellido}</StyledTableCell>
+            <StyledTableCell>{usuario.email}</StyledTableCell>
+            <StyledTableCell>{usuario.telefono}</StyledTableCell>
+            <StyledTableCell>
               {usuario.id % 2 === 0 ? (
                 <DoneIcon fontSize="large" />
               ) : (
                 <ClearIcon fontSize="large" />
               )}
-            </td>
-            <td>
+            </StyledTableCell>
+            <StyledTableCell>
               <ModalDeshabilitarUsuario
                 cambiarEstadoUsuario={() =>
                   // eslint-disable-next-line no-use-before-define
@@ -64,40 +127,10 @@ const PantallaUsuarios = () => {
                   onSubmit={editarUsuario}
                 />
               )}
-            </td>
-          </tr>
+            </StyledTableCell>
+          </StyledTableRow>
         ))}
-      </tbody>
-    );
-  };
-
-  const cambiarEstadoUsuario = (usuarioId, estadoViejo) => {
-    console.log(usuarioId);
-    console.log(estadoViejo);
-  };
-
-  const editarUsuario = (usuarioId, usuario) => {
-    console.log(usuario);
-  };
-
-  const renderTableHeaders = () => {
-    const headers = [
-      "Id",
-      "Nombre",
-      "Apellido",
-      "Email",
-      "Teléfono",
-      "Habilitado",
-      "Acciones",
-    ];
-    return (
-      <thead>
-        <tr>
-          {headers.map((header) => (
-            <th key={header}>{header}</th>
-          ))}
-        </tr>
-      </thead>
+      </TableBody>
     );
   };
 
@@ -106,17 +139,16 @@ const PantallaUsuarios = () => {
       <Typography variant="h3" gutterBottom>
         Estado
       </Typography>
-      <div>
-        <div className="row col-8">
-          {error.hayError && <Alert variant="danger">{error.mensaje}</Alert>}
-          {usuarios && (
-            <Table striped bordered hover>
-              {renderTableHeaders()}
-              {renderTableBody()}
-            </Table>
-          )}
-        </div>
-      </div>
+      {error.hayError && <Alert severity="error">{error.mensaje}</Alert>}
+      <br />
+      <TableContainer component={Paper}>
+        {usuarios && (
+          <Table className={classes.table} aria-label="customized table">
+            {renderTableHeaders()}
+            {renderTableBody()}
+          </Table>
+        )}
+      </TableContainer>
     </Container>
   );
 };
